a2f4948bc362561e30ddd3c11bd4b56f098f0344
Mark Sawyer <msawye16@ford.com>
2019-03-28 00:03:58 -0400

    Beginning MVC design to pull together all StateEngineCrank modules and simulations.

---
+++ source/main.py
@@ -0,0 +1,23 @@
+""" StateEngineCrank Model-View-Controller Main Module
+
+Functions as the Controller for the StateEngineCrank MVC implementation
+
+Provides control for:
+
+* StateEngineCrank execution
+* DiningPhilosophers simulation
+* SleepingBarber simulation
+
+"""
+
+# System imports
+
+# Project imports
+
+if __name__ == '__main__':
+
+    # setup model-view-controller relationships
+    pass
+
+    # launch the view module
+    pass
