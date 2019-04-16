106c2da3febb2128b011f90df72025b6fc053f10
Mark Sawyer <msawye16@ford.com>
2019-04-16 00:00:24 -0400

    backup work in progress

--- source/tk_main.py
+++ source/tk_main.py
@@ -7,26 +7,48 @@ class MyTk():
     def __init__(self):
         self.root = Tk()
         self.root.title('MyTk Root')
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
 
-        self.mainframe = ttk.Frame(self.root, padding="3 3 12 12")
-        self.mainframe.columnconfigure(0, weight=1)
-        self.mainframe.rowconfigure(0, weight=1)
-        self.mainframe['relief'] = 'raised'
+        # ------------------------------------------------
+        # main frame - lives inside TkRoot
+        # row 0: mainframe label
+        self.mainframe = ttk.Frame(self.root, padding="10 10 10 10")
+        self.mainframe.grid(row=1, column=0, sticky=(N, W, E, S))
+        self.mainframe.grid_rowconfigure(1, weight=1)
+        self.mainframe.grid_columnconfigure(0, weight=1)
         self.mainframe['borderwidth'] = 4
-        self.mainframe.grid(sticky=(N, W, E, S))
-        self.mainframe_label = Label(self.mainframe, text='MainFrame Label')
-        self.mainframe_label.grid(sticky=(N, W, E, S))
-        self.mainframe_label['borderwidth'] = 4
+        self.mainframe['relief'] = 'raised'
 
-        self.ani_frame = ttk.Frame(self.mainframe, padding="3 3 12 12")
-        self.ani_frame.grid(sticky=(N, W, E, S))
-        self.ani_frame.columnconfigure(0, weight=1)
-        self.ani_frame.rowconfigure(0, weight=1)
-        self.ani_frame['relief'] = 'raised'
+        self.mainframe_label = Label(self.mainframe, text='MainFrame Label')
+        self.mainframe_label.grid(row=0, column=0, sticky=(N, W, E, S))
+        self.mainframe_label.grid_rowconfigure(0, weight=1)
+        self.mainframe_label.grid_columnconfigure(0, weight=1)
+        self.mainframe_label['borderwidth'] = 4
+        self.mainframe_label['relief'] = 'sunken'
+
+        # -----------------------------------------------
+        # animation frame - lives inside mainframe
+        # row 0: label
+        # row 1: console
+        self.ani_frame = ttk.Frame(self.mainframe, padding="10 10 10 10")
+        self.ani_frame.grid(row=1, column=0, sticky=(N, W, E, S))
+        self.ani_frame.grid_rowconfigure(1, weight=1)
+        self.ani_frame.grid_columnconfigure(0, weight=1)
         self.ani_frame['borderwidth'] = 4
-        self.ani_frame_label = Label(self.mainframe, text='AniFrame Label')
-        self.ani_frame_label.grid(sticky=(N, W, E, S))
+        self.ani_frame['relief'] = 'raised'
+
+        self.ani_frame_label = Label(self.ani_frame, text='AniFrame Label')
+        self.ani_frame_label.grid(row=0, column=0, sticky=(N, W, E, S))
         self.ani_frame_label['borderwidth'] = 4
+        self.ani_frame_label['relief'] = 'sunken'
+
+        self.ani_console = Text(self.ani_frame, name='ani_console')
+        self.ani_console.grid(row=1, column=0, sticky=(N, W, E, S))
+        self.ani_console.grid_rowconfigure(1, weight=1)
+        self.ani_console.grid_columnconfigure(0, weight=1)
+        self.ani_console['relief'] = 'raised'
+
 
 myTk = MyTk()
 myTk.root.mainloop()
