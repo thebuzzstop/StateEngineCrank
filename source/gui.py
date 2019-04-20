b6460452c541f0b51f2822b3aea39b4f56a59f18
Mark Sawyer <msawye16@ford.com>
2019-04-20 01:44:17 -0400

    Commit work-in-progress. Width resizing good. Heigh resizing still f'd up.

--- source/gui.py
+++ source/gui.py
@@ -1,7 +1,6 @@
 """ StateEngineCrank Tkinter GUI View """
 
 # System imports
-import datetime
 import threading
 from tkinter import *
 from tkinter import ttk
@@ -22,56 +21,57 @@ class Animation(mvc.View):
         self.mainframe = mainframe
         self.config = config
         self.common = common
-        self.col = self.config['column']
 
-        # Mainframe
-        self.mainframe.grid(row=0, column=self.config['column'], sticky=self.common['mainframe.stick'])
+        # Configure our column in mainframe
+        self.ani_frame_column = self.config['column']
+        self.mainframe.grid_columnconfigure(self.config['column'], weight=1)
+
+        # Establish the rows of our animation layout
+        # Row: 0 - the only row of the top-level frame
+        self.ani_frame_row_ = -1
+        self.ani_frame_row = self._next_ani_frame_row()
 
         # Top level frame for this simulation
-        self.ani_frame_row_ = -1
-        self.ani_frame = ttk.Frame(self.mainframe, padding="8 8 8 8", name='ani_frame%s' % self.col)
+        self.ani_frame = ttk.Frame(self.mainframe, padding='4 4 4 4', name=self.name_rc('ani_frame'))
+        self.ani_frame['relief'] = 'raised'
         self.ani_frame['borderwidth'] = 4
-        self.ani_frame['relief'] = 'sunken'
+        # row=0, top-most row of the animation frame
+        self.ani_frame.grid(row=self.ani_frame_row, column=self.ani_frame_column, sticky=self.common['ani.frame.stick'])
+        self.ani_frame.grid_columnconfigure(0, weight=1)
 
-        self.ani_frame_row = self._next_animation_frame_row()
-        self.ani_frame.grid(row=self.ani_frame_row, column=config['column'], sticky=self.common['frame.stick'])
-        self.ani_frame.rowconfigure(self.ani_frame_row, weight=1)
-
-        self.ani_label = Label(self.ani_frame, text=config['title'], name='ani_label%s' % self.col)
-        self.ani_label_row = self._next_animation_frame_row()
-        self.ani_label.grid(row=self.ani_label_row, column=config['column'], sticky=self.common['label.stick'])
+        self.ani_frame_label = Label(self.ani_frame, text=config['title'], name=self.name_rc('ani_frame_label'))
+        self.ani_frame_label_row = self._next_ani_frame_row()   # first row of ani_frame
+        self.ani_frame_label.grid(row=self.ani_frame_label_row, sticky=self.common['label.stick'])
 
         # Top level frame for this simulation animation
-        self.ani_animation_frame = ttk.Frame(self.ani_frame, padding="3 3 12 12",
-                                             name='ani_animation_frame%s' % self.col)
-        self.ani_animation_frame_row = self._next_animation_frame_row()
-        self.ani_animation_frame.grid(row=self.ani_animation_frame_row, column=config['column'],
-                                      sticky=self.common['animation.stick'])
-        self.ani_animation_frame['borderwidth'] = 4
-        self.ani_animation_frame['relief'] = 'sunken'
+        self.ani_graphics_frame = ttk.Frame(self.ani_frame, padding='4 4 4 4',
+                                            name=self.name_rc('ani_graphics_frame'))
+        self.ani_graphics_frame['borderwidth'] = 4
+        self.ani_graphics_frame['relief'] = 'sunken'
+        self.ani_graphics_frame_row = self._next_ani_frame_row()
+        self.ani_graphics_frame.grid(row=self.ani_graphics_frame_row, sticky=self.common['animation.stick'])
+        self.ani_graphics_frame.grid_rowconfigure(self.ani_graphics_frame_row, weight=1)
 
-        self.ani_animation_title = Label(self.ani_animation_frame, text=self.config['animation.text'],
-                                         name='ani_animation_text%s' % self.col)
-        self.ani_animation_title.grid()
+        self.ani_animation_title = Label(self.ani_graphics_frame, text=self.config['animation.text'],
+                                         name=self.name_rc('ani_animation_text'))
+        self.ani_animation_title.grid(sticky='n')
 
         # Define a canvas where animation graphics can be drawn
-        self.ani_canvas = Canvas(self.ani_animation_frame,
+        self.ani_canvas = Canvas(self.ani_graphics_frame,
                                  width=self.common['animation']['width'],
                                  height=self.common['animation']['height'],
-                                 name='ani_canvas%s' % self.col)
-        self.ani_canvas.grid()
+                                 name=self.name_rc('ani_canvas'))
+        self.ani_canvas.grid(sticky='n')
 
         # Calculate the center of the animation canvas
         self.ani_center = (self.common['animation']['width']/2, self.common['animation']['height']/2)
 
-        # Buttons and controls frame
-        self.ani_buttons_frame = ttk.Frame(self.ani_frame, name='ani_buttons_frame%s' % self.col)
-        self.ani_buttons_frame_row = self._next_animation_frame_row()
-        self.ani_buttons_frame.grid(row=self.ani_buttons_frame_row,
-                                    column=config['column'],
-                                    sticky=self.common['buttons.stick'])
+        # Buttons and controls start a new frame
+        self.ani_buttons_frame = ttk.Frame(self.ani_frame, padding="4 4 4 4", name=self.name_rc('ani_buttons_frame'))
         self.ani_buttons_frame['borderwidth'] = 4
         self.ani_buttons_frame['relief'] = 'raised'
+        self.ani_buttons_frame_row = self._next_ani_frame_row()
+        self.ani_buttons_frame.grid(row=self.ani_buttons_frame_row, sticky=self.common['buttons.stick'])
 
         self.buttons = [
             ['Start', self._button_start],
@@ -83,41 +83,34 @@ class Animation(mvc.View):
 
         self.ani_buttons = {}
         column = -1
-        self.button_row = self._next_animation_frame_row()
         for b in self.buttons:
             column += 1
             self.ani_buttons[b[0]] = ttk.Button(self.ani_buttons_frame, text=b[0], command=b[1])
-            self.ani_buttons[b[0]].grid(row=self.button_row, column=column)
+            self.ani_buttons[b[0]].grid(row=0, column=column)
 
-        # Console for text and logging output
-        self.ani_console_label = Label(self.ani_frame, text='Console Log', name='ani_console_label%s' % self.col)
-        self.ani_console_label_row = self._next_animation_frame_row()
-        self.ani_console_row = self.ani_console_label_row
-        self.ani_console_label.grid(row=self.ani_console_row,
-                                    column=self.col,
-                                    sticky=self.common['console.label.stick'])
-        self.ani_console = Text(self.ani_frame,
-                                height=self.common['console']['height'],
-                                width=self.common['console']['width'],
-                                name='ani_console%s' % self.col)
-        self.ani_console_row = self._next_animation_frame_row()
-        self.ani_console.grid(row=self.ani_console_row,
-                              column=self.col,
-                              sticky=self.common['console.stick'])
-        self.ani_console.rowconfigure(self.ani_console_row, weight=2)
+        # Console for text and logging output starts a new frame
+        self.ani_console_frame = ttk.Frame(self.ani_frame, padding="4 4 4 4", name=self.name_rc('ani_console_frame'))
+        self.ani_console_frame_row = self._next_ani_frame_row()
+        self.ani_console_frame.grid(row=self.ani_console_frame_row, sticky=self.common['console.frame.stick'])
+        self.ani_console_frame.grid_rowconfigure(self.ani_console_frame_row, weight=1)
+        self.ani_console_frame.grid_columnconfigure(0, weight=1)
+        self.ani_console_frame['borderwidth'] = 4
+        self.ani_console_frame['relief'] = 'raised'
+        self.ani_console_label_row = 0
+        self.ani_console_text_row = 1
+        self.ani_console_label = Label(self.ani_console_frame, text='Console Log',
+                                       name=self.name_rc('ani_console_label'))
+        self.ani_console_label.grid(row=self.ani_console_label_row, sticky=self.common['console.label.stick'])
+        self.ani_console_text = Text(self.ani_console_frame,
+                                     height=self.common['console']['height'],
+                                     width=self.common['console']['width'],
+                                     name=self.name_rc('ani_console_text'))
+        self.ani_console_text.grid(row=self.ani_console_text_row, sticky=self.common['console.stick'])
+        self.ani_console_text.grid_rowconfigure(self.ani_console_text_row, weight=1)
+        self.ani_console_text.insert('2.0', config['console.text'])
+        self.ani_console_text.insert(END, '\n\n')
 
-        self.ani_console.insert('2.0', config['console.text'])
-        self.ani_console.insert(END, '\n\n')
-
-        # Set frame weights
-        self.mainframe.grid_columnconfigure(self.col, weight=1)
-        self.ani_animation_frame.grid_rowconfigure(self.ani_animation_frame_row, weight=1)
-        self.ani_animation_frame.grid_columnconfigure(self.col, weight=1)
-        self.ani_frame.grid_rowconfigure(self.ani_frame_row, weight=1)
-        self.ani_frame.grid_columnconfigure(self.col, weight=1)
-        self.ani_console.grid_rowconfigure(self.ani_console_row, weight=1)
-        self.ani_console.grid_columnconfigure(self.col, weight=1)
-
+        # Animation Button Events
         self.mvc_events = mvc.Event()
         self.mvc_events.register_class(config['event.class'])
         self.mvc_events.register_event(config['event.class'], 'Start', event_type='view', text='Start')
@@ -127,6 +120,9 @@ class Animation(mvc.View):
         self.mvc_events.register_event(config['event.class'], 'Resume', event_type='view', text='Resume')
         self.mvc_events.register_event(config['event.class'], 'Logger', event_type='view', text='Logger')
 
+    def name_rc(self, base):
+        return '%s-%s-%s' % (base, self.ani_frame_row_, self.ani_frame_column)
+
     def run(self):
         """ Satisfy base class requirements """
         pass
@@ -135,7 +131,7 @@ class Animation(mvc.View):
         """ Satisfy base class requirements """
         pass
 
-    def _next_animation_frame_row(self):
+    def _next_ani_frame_row(self):
         self.ani_frame_row_ += 1
         return self.ani_frame_row_
 
@@ -309,13 +305,14 @@ class GuiView(mvc.View):
 
     common_config = {
         'mainframe.stick': (N, S, E, W),
-        'animation': {'width': 360, 'height': 360},
+        'animation': {'width': 320, 'height': 320},
         'animation.stick': N,
         'buttons.stick': N,
         'console': {'width': 40, 'height': 10},
         'console.stick': (N, S, E, W),
         'console.label.stick': N,
-        'frame.stick': (N, S, E, W),
+        'console.frame.stick': (N, S, E, W),
+        'ani.frame.stick': (N, S, E, W),
         'label.stick': N,
     }
 
@@ -356,6 +353,11 @@ class GuiView(mvc.View):
         self.mainframe = None
         self.gui_thread = None
 
+        self.tkobj_dictionary = {}
+        self.tkobj_dictionary2 = {}
+
+        self.tkobj_deltas = []
+
     def update(self, event):
         """ Called to let us know of an event
 
@@ -373,6 +375,13 @@ class GuiView(mvc.View):
             pass
         else:
             print(event)
+        print('==========================================================================================')
+        self.dump_tkinfo(1, self.root)
+
+        print('---------------------------------')
+        for tkobj in self.tkobj_dictionary.keys():
+            if tkobj in self.tkobj_dictionary2.keys():
+                print('{}\n{}\n{}\n'.format(tkobj, self.tkobj_dictionary[tkobj], self.tkobj_dictionary2[tkobj]))
 
     def update_gui(self, gui, event):
         # print('%s[%s] %s -> %s' % (event.sm_name, event.sm_event, event.sim_event, event.sim_state))
@@ -405,20 +414,75 @@ class GuiView(mvc.View):
     def write(self, text):
         print(text)
 
+    def dump_tkinfo(self, level, tkobj):
+
+        parent = 'unknown'
+        name = 'noname'
+        height = 'unknown'
+        width = 'unknown'
+        sticky = '-'
+        row = '-'
+        col = '-'
+        grid_size = '(c, r)'
+
+        if hasattr(tkobj, 'winfo_width'):
+            width = tkobj.winfo_width()
+        if hasattr(tkobj, 'winfo_height'):
+            height = tkobj.winfo_height()
+
+        if hasattr(tkobj, 'grid_info'):
+            grid_info = tkobj.grid_info()
+            row = grid_info['row']
+            col = grid_info['column']
+            sticky = grid_info['sticky']
+            grid_size = tkobj.grid_size()
+        if hasattr(tkobj, 'master'):
+            if hasattr(tkobj.master, '_name'):
+                parent = tkobj.master._name
+
+        if hasattr(tkobj, '_name'):
+            name = tkobj._name
+            if name not in self.tkobj_dictionary:
+                self.tkobj_dictionary[name] = {'width': width, 'height': height}
+            else:
+                if width != self.tkobj_dictionary[name]['width'] or height != self.tkobj_dictionary[name]['height']:
+                    self.tkobj_dictionary2[name] = {'width': width, 'height': height}
+
+        level_ = '+'*level
+        level_ = level_ + ' '*(6-level)
+
+        print('Level{}{} wininfo [{:4}/{:4}] row/column: {}/{} sticky: {:4} grid: {} name: {:24} parent: {:24}'
+              .format(level, level_, width, height, row, col, sticky, grid_size, name, parent))
+
+        # recurse through all children
+        for child in tkobj.children.keys():
+            self.dump_tkinfo(level + 1, tkobj.children[child])
+
+    def dict_info(self, dict_):
+        for key in dict(dict_).keys():
+            value_ = dict_[key]
+            type_ = str(type(value_))
+            print('{:20} : {:10} {}'.format(key, type_, value_))
+
     def tk_run(self):
         """ GUI view running - setup basic framework """
         self.root = Tk()
         self.root.title(Defines.TITLE)
+        self.root.config(bg='red')
+        # root
+        #   * an expanding box, i.e.
+        #   * 1 row, weight=1
+        #   * 1 column, weight=1
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
 
-        self.mainframe = ttk.Frame(self.root, padding="10 10 10 10", name='mainframe')     # width=800, height=800)
-        #self.mainframe.grid(row=0, column=0, sticky=(N, W, E, S))
-        #self.mainframe.grid(row=0, column=1, sticky=(N, W, E, S))
-        #self.mainframe.grid_rowconfigure(0, weight=1)
-        #self.mainframe.grid_columnconfigure(0, weight=1)
-        #self.mainframe.grid_rowconfigure(1, weight=1)
-        #self.mainframe.grid_columnconfigure(1, weight=1)
+        # mainframe:
+        #   * 1 row, weight=1
+        #   * 'n' columns, weight=1
+        #   * columns are configured by the animations that occupy them
+        self.mainframe = ttk.Frame(self.root, relief='sunken', padding="10 10 10 10", name='mainframe')
+        self.mainframe.grid(row=0, column=0, sticky=(N, W, E, S))
+        self.mainframe.grid_rowconfigure(0, weight=1)
 
         # instantiate our GUI animation views
         ani_dining = DiningPhilosophers(parent=self, root=self.root, mainframe=self.mainframe,
@@ -448,6 +512,13 @@ class GuiView(mvc.View):
         ani_barbers.add_waiting_room()
         for b in ani_barbers.ani_buttons.keys():
             ani_barbers.ani_buttons[b].state(['disabled'])
+
+        print('==========================================================================================')
+        self.dict_info(self.root)
+        print('=========================================')
+        self.dict_info(self.mainframe)
+        print('=========================================')
+        self.dict_info(self.mainframe.children['ani_frame-0-0'])
 
         # all setup so run
         self.root.mainloop()
--- source/tk_main.py
+++ source/tk_main.py
@@ -8,14 +8,16 @@ class MyTk():
         # ------------------------------------------------
         # main frame - lives inside TkRoot
         # row 0: mainframe label
-        mainframe = ttk.Frame(self.root, padding="10 10 10 10")
+        mainframe = ttk.Frame(self.root, padding="10 10 10 10",
+                              name='mainframe%s' % column)
         mainframe.grid(row=0, column=column, sticky=(N, W, E, S))
         mainframe.grid_rowconfigure(1, weight=1)
         mainframe.grid_columnconfigure(column, weight=1)
         mainframe['borderwidth'] = 4
         mainframe['relief'] = 'raised'
 
-        mainframe_label = Label(mainframe, text='MainFrame Label')
+        mainframe_label = Label(mainframe, text='MainFrame Label',
+                                name='mainframe_label%s' % column)
         mainframe_label.grid(row=0, column=column, sticky=(N, W, E, S))
         mainframe_label.grid_rowconfigure(0, weight=1)
         mainframe_label.grid_columnconfigure(column, weight=1)
@@ -26,19 +28,21 @@ class MyTk():
         # animation frame - lives inside mainframe
         # row 0: label
         # row 1: console
-        ani_frame = ttk.Frame(mainframe, padding="10 10 10 10")
+        ani_frame = ttk.Frame(mainframe, padding="10 10 10 10",
+                              name='ani_frame%s' % column)
         ani_frame.grid(row=1, column=column, sticky=(N, W, E, S))
         ani_frame.grid_rowconfigure(1, weight=1)
         ani_frame.grid_columnconfigure(column, weight=1)
         ani_frame['borderwidth'] = 4
         ani_frame['relief'] = 'raised'
 
-        ani_frame_label = Label(ani_frame, text='AniFrame Label')
+        ani_frame_label = Label(ani_frame, text='AniFrame Label',
+                                name='ani_frame_label%s' % column)
         ani_frame_label.grid(row=0, column=column, sticky=(N, W, E, S))
         ani_frame_label['borderwidth'] = 4
         ani_frame_label['relief'] = 'sunken'
 
-        ani_console = Text(ani_frame, name='ani_console')
+        ani_console = Text(ani_frame, name='ani_console%s' % column)
         ani_console.grid(row=1, column=column, sticky=(N, W, E, S))
         ani_console.grid_rowconfigure(1, weight=1)
         ani_console.grid_columnconfigure(column, weight=1)
