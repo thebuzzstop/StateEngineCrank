856ceec74722d5bc76f2356af838e176bb601bce
Mark Sawyer <mbs@mysawyer.net>
2023-12-01 23:29:56 -0500

    initial commit before bookmark verify changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='StateEngineCrank',
-    version='0.1',
+    version='0.1.1',
     packages=setuptools.find_packages(),
     package_dir={'': ''},
     url='https://github.com/thebuzzstop/StateEngineCrank',
