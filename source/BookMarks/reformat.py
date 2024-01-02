6ff339428541db4e7efff6515a1a8b68c03738df
Mark Sawyer <mbs@mysawyer.net>
2024-01-02 00:49:34 -0500

    Checkpoint: cosmetic changes to doc-strings

--- source/BookMarks/reformat.py
+++ source/BookMarks/reformat.py
@@ -1,7 +1,6 @@
-""" BookMarks Reformat module
+"""BookMarks Reformat module
 
 The BookMarks reformat module reorders and reorganizes the bookmarks.
-
 """
 
 # System imports
@@ -20,10 +19,10 @@ from logger import Logger
 logger = Logger(name=__name__).logger
 
 class Reformat:
-    """ Class to reformat bookmarks """
+    """Class to reformat bookmarks"""
 
     def __init__(self):
-        """ Reformat constructor"""
+        """Reformat constructor"""
         logger.info('INIT (%s)', __name__)
 
         self.headings = TheConfig.headings                  #: users headings configuration
@@ -51,9 +50,9 @@ class Reformat:
         return self._output
 
     def write_section(self, section):
-        """ write entire section to output string
+        """write entire section to output string
 
-            :param section: bookmarks section to output
+        :param section: bookmarks section to output
         """
         # see if current section has a heading
         if section in self.headings:
@@ -85,12 +84,12 @@ class Reformat:
                 self.write_bm(bm)
 
     def output_subsection(self, section, subsection):
-        """ output bookmarks for section.subsection
+        """output bookmarks for section.subsection
 
-            Special case processing for *www* section.
+        Special case processing for *www* section.
 
-            :param section: Active section name
-            :param subsection: Active subsection name
+        :param section: Active section name
+        :param subsection: Active subsection name
         """
         # special case of 'www' section
         if section == 'www':
@@ -141,19 +140,19 @@ class Reformat:
 
     @staticmethod
     def has_heading(section, subsection):
-        """ function to determine if subsection has a heading
+        """function to determine if subsection has a heading
 
-            :param section: Active section name
-            :param subsection: Active subsection name
-            :return: True if section.subsection has a heading
+        :param section: Active section name
+        :param subsection: Active subsection name
+        :return: True if section.subsection has a heading
         """
         return f'{section}.{subsection}' not in TheConfig.noheadings
 
     def write_bm(self, bm: BookMark, has_label=True):
-        """ write a single bookmark to output string
+        """write a single bookmark to output string
 
-            :param bm: Bookmark to output
-            :param has_label: Bookmark has a label
+        :param bm: Bookmark to output
+        :param has_label: Bookmark has a label
         """
         if has_label:
             if 'icon' in bm.attrs:
@@ -187,47 +186,46 @@ class Reformat:
         pass
 
     def write_heading(self, heading: str):
-        """ write heading data to output string
+        """write heading data to output string
 
-            :param heading: heading text
+        :param heading: heading text
         """
         self.write(TheConfig.HEADING_HTML_FORMAT.format(self.datestamp, self.datestamp, self.my_title(heading)))
 
     def write_toolbar_heading(self, heading: str):
-        """ write heading data to output string
+        """write heading data to output string
 
-            :param heading: heading text
+        :param heading: heading text
         """
         self.write(TheConfig.TOOLBAR_HTML_FORMAT.format(self.datestamp, self.datestamp, self.my_title(heading)))
 
     def begin_list(self):
-        """ starts an HTML list and bumps the indent level """
+        """starts an HTML list and bumps the indent level"""
         self.write(TheConfig.LIST_HTML)
         self.indent += 1
 
     def end_list(self):
-        """ decrements the indent level and ends an HTML list """
+        """decrements the indent level and ends an HTML list"""
         self.indent -= 1
         self.write(TheConfig.LIST_HTML_END)
 
     def write(self, html: Union[str, list]):
-        """ write html to output string
+        """write html to output string
 
-            :param html: html string(s) to output, may be a list
+        :param html: html string(s) to output, may be a list
         """
         if isinstance(html, list):
             for h in html:
                 self.write(h)
             return
         self._output.append('    '*self.indent + html.strip())
-        pass
 
     @staticmethod
     def my_title(heading):
-        """ format a heading string the way we like it
+        """format a heading string the way we like it
 
-            :param heading: Heading string to format
-            :return: Heading formatted as we like it
+        :param heading: Heading string to format
+        :return: Heading formatted as we like it
         """
         heading = heading.title()
         for original, replacement in TheConfig.capitalized:
